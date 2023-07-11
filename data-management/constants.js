a21c84b6162efd754ce77d85686d7d4b78669f40
Jonatas <jonatas1503@hotmail.com>
2023-07-10 21:20:40 -0300

    updates infos

--- data-management/constants.js
+++ data-management/constants.js
@@ -3,7 +3,7 @@ const { request, stash, spec } = require('pactum'),
 
 /** O '$M{variavel}' refere-se a um mapa de dados que carrega o conteúdo daquela váriavel*/
 request.setBaseUrl('$M{Environments.Production}')
-request.setDefaultTimeout(5000)
+request.setDefaultTimeout(10000)
 /** loadData - Carrega modelos de dados e mapas de dados do sistema de arquivos */
 stash.loadData('./data-management/data')
 
